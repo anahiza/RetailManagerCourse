aa9cfe9faef403003e7b95d25b7f2a385258369b
Anahi <anahi.z.a@gmail.com>
2021-09-05 21:44:09 -0500

    Catch error in login

--- TRMDesktopUserInterface/Bootstrapper.cs
+++ TRMDesktopUserInterface/Bootstrapper.cs
@@ -30,7 +30,8 @@ namespace TRMDesktopUserInterface
             _container.Instance(_container);
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
